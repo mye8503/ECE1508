b3b77e2e10eec6980dcd384d5d4f48d55b90784c
“mainbrm1380” <mainbrm1380@gmail.com>
2025-11-17 20:27:01 -0500

    Add dataset and code to get it

---
+++ get_data.py
@@ -0,0 +1,31 @@
+import yfinance
+import pandas as pd
+import numpy as np
+
+# stocks are: apple, google, microsoft, amazon, nvidia
+# stock_list = ["AAPL", "META", "GOOG", "MSFT", "AMZN"]
+stock_list = ["AAPL", "GOOG", "MSFT", "AMZN", "NVDA"]
+
+# 6000 thousand days of observations are collected, which is roughly equivalent to from January of 2005 to January of 2025.
+start_date = "2005-01-01"
+end_date = "2025-01-01"
+
+# download the data
+data = yfinance.download(stock_list, start=start_date, end=end_date)
+
+# number of na vals in the data
+missing_data = len(data[data.isna().any(axis=1)])
+
+# make sure there are no missing values
+if missing_data > 0:
+    raise ValueError(f"There are missing values in the dataset!")
+
+# add the log return of each of the stocks
+for stock in stock_list:
+    data["Log return", stock]= np.log(data["Close"][stock]).diff()
+
+print(len(data))  # there are 5033 data points for each stock
+print(data.columns)
+
+# save as csv
+data.to_csv("dataset.csv")
