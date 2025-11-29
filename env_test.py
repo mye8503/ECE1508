7f59d564bdbc5d157db0ed77db0fd919dc12ec45
Maggie Ye <106449396+mye8503@users.noreply.github.com>
2025-11-29 15:18:32 -0500

    fix another problem with dates and add total number of stocks owned

--- env_test.py
+++ env_test.py
@@ -6,7 +6,7 @@ import pickle
 
 class TestStockTradingEnv(gym.Env):
 
-    def __init__(self, num_stocks=5, lookback_period=60, dataset_path='dataset.pkl'):
+    def __init__(self, num_companies=5, num_stocks=1000, lookback_period=60, dataset_path='dataset.pkl'):
         # load dataset from pickle file
         self.dataset_path = dataset_path
         self.data = self._load_dataset()
@@ -15,41 +15,44 @@ class TestStockTradingEnv(gym.Env):
         self.data_open = self.data['Open']
 
         # number of stocks to trade
+        self.num_companies = num_companies
         self.num_stocks = num_stocks
 
         # set start date for the environment
         day = pd.Timedelta(days=1)
         self.start_date = self.data.index[0] + day * lookback_period
+        
         self.current_date = self.start_date
+        self.check_omitted_dates()
 
         self.lookback_period = lookback_period
 
         # array representing each stock's fraction of the total portfolio
         # initialize with equal allocation
-        self._portfolio_allocation = np.array([1/self.num_stocks] * self.num_stocks)
+        self._portfolio_allocation = np.array([1/self.num_companies] * self.num_companies)
 
         # define what the agent can observe
         # dictionary containing current portfolio allocation plus
         # stock log returns for the duration of the lookback period
         # plus volatility metrics (optional)
         self.observation_space = gym.spaces.Dict({
-            'portfolio_allocation': gym.spaces.Box(low=0, high=1, shape=(self.num_stocks,), dtype=np.float32),
-            'stock_lrs': gym.spaces.Box(low=-np.inf, high=np.inf, shape=(self.num_stocks, lookback_period), dtype=np.float32),
-            'stock_opens': gym.spaces.Box(low=0, high=np.inf, shape=(self.num_stocks, lookback_period), dtype=np.float32),
-            # 'volatility_metrics': gym.spaces.Box(low=0, high=np.inf, shape=(self.num_stocks, lookback_period), dtype=np.float32)
+            'portfolio_allocation': gym.spaces.Box(low=0, high=1, shape=(self.num_companies,), dtype=np.float32),
+            'stock_lrs': gym.spaces.Box(low=-np.inf, high=np.inf, shape=(self.num_companies, lookback_period), dtype=np.float32),
+            'stock_opens': gym.spaces.Box(low=0, high=np.inf, shape=(self.num_companies, lookback_period), dtype=np.float32),
+            # 'volatility_metrics': gym.spaces.Box(low=0, high=np.inf, shape=(self.num_companies, lookback_period), dtype=np.float32)
         })
 
         # define what actions are available
         # sum must be 1 (100% of portfolio)
-        self.action_space = gym.spaces.Box(low=0, high=1, shape=(self.num_stocks,), dtype=np.float32)
+        self.action_space = gym.spaces.Box(low=0, high=1, shape=(self.num_companies,), dtype=np.float32)
 
     
     def _get_obs(self):
         # return the current observation
         return {
             'portfolio_allocation': self._portfolio_allocation,
-            'stock_lrs': self._get_stock_lrs(),
-            'stock_opens': self._get_stock_opens(),
+            'stock_lrs': [] if self.current_date is None else self._get_stock_lrs(),
+            'stock_opens': [] if self.current_date is None else self._get_stock_opens(),
             'current_date': self.current_date
             # 'volatility_metrics': self._get_volatility_metrics()
         }
@@ -59,12 +62,13 @@ class TestStockTradingEnv(gym.Env):
         super().reset(seed=seed)
 
         # reset portfolio allocation to equal distribution
-        self._portfolio_allocation = np.array([1/self.num_stocks] * self.num_stocks)
+        self._portfolio_allocation = np.array([1/self.num_companies] * self.num_companies)
 
         # reset log returns
         self.data_lrs = self._get_stock_lrs()
 
         self.current_date = self.start_date
+        self.check_omitted_dates()
 
         # return initial observation
         return self._get_obs(), {}
@@ -91,7 +95,6 @@ class TestStockTradingEnv(gym.Env):
     
 
     def _load_dataset(self):
-        print("hello", self.dataset_path)
         # load dataset from pickle file
         with open(self.dataset_path, 'rb') as f:
             data = pickle.load(f)
@@ -102,8 +105,6 @@ class TestStockTradingEnv(gym.Env):
         # get stock prices for the lookback period
         start_date = self.subtract_dates(self.current_date, self.lookback_period)
 
-        print("hello", start_date, self.current_date)
-        # print(self.data_lrs[start_date:current_date])
         return self.data_lrs[start_date:self.current_date]
     
 
@@ -111,8 +112,6 @@ class TestStockTradingEnv(gym.Env):
         # get stock prices for the lookback period
         start_date = self.subtract_dates(self.current_date, self.lookback_period)
 
-        print("hello", start_date, self.current_date)
-        # print(self.data_open[start_date:current_date])
         return self.data_open[start_date:self.current_date]
     
 
@@ -126,8 +125,12 @@ class TestStockTradingEnv(gym.Env):
         yesterday_open = self.data_open[yesterday:yesterday]
         yesterday_open = np.array(yesterday_open)[0]
 
-        old_value = np.dot(self._portfolio_allocation, yesterday_open)
-        new_value = np.dot(new_allocation, today_open)
+        old_value = np.dot(self._portfolio_allocation * self.num_stocks, yesterday_open)
+        new_value = np.dot(new_allocation * self.num_stocks, today_open)
+
+        if self.current_date.day % 10 == 0:
+            print(f"Date: {self.current_date}, Old Value: {old_value}, New Value: {new_value}, Reward: {new_value - old_value}")
+
         reward = new_value - old_value
         return reward
     
@@ -135,7 +138,18 @@ class TestStockTradingEnv(gym.Env):
     def subtract_dates(self, date, num_days):
         day = pd.Timedelta(days=1)
 
-        return date - num_days * day
+        newdate = date - num_days * day
+
+        # check if this is an omitted date
+        while newdate not in self.data_open.index or len(self.data_open[newdate:newdate]) == 0:
+            newdate -= day
+
+            # check if we have finished the dataset
+            if newdate < self.data_open.index[0]:
+                newdate += day
+                return  
+
+        return newdate
 
 
     def increment_date(self):
@@ -143,12 +157,17 @@ class TestStockTradingEnv(gym.Env):
 
         self.current_date += day
 
+        self.check_omitted_dates()
+
+    
+    def check_omitted_dates(self):
+        day = pd.Timedelta(days=1)
+
         # check if this is an omitted date
-        while self.current_date not in self.data_open.index:
+        while self.current_date not in self.data_open.index or len(self.data_open[self.current_date:self.current_date]) == 0:
             self.current_date += day
 
             # check if we have finished the dataset
             if self.current_date > self.data_open.index[-1]:
                 self.current_date = None
                 return  
-
