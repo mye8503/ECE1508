40208ba54d1a79a3902e324a318d88b368fa9f2c
Maggie Ye <106449396+mye8503@users.noreply.github.com>
2025-12-03 13:00:35 -0500

    equal weighting done and test results done

--- Equal_Weighting.py
+++ Equal_Weighting.py
@@ -1,59 +1,93 @@
-import torch
-import pygame
 import numpy as np
-import gymnasium as gym
+from numpy import linalg as LA
 import matplotlib.pyplot as plt
+import pickle
+import os
 
-from Hyperparameters import Hyperparameters
+from ppo_env import StockTradingEnv
 
-# implement a model to trade stocks using the equal weighting strategy
-class EQ_Agent():
-    def __init__(self, hyperparameters:Hyperparameters):
-        self.hp = hyperparameters
+from backtest_ppo import plot_annual_returns
+from backtest_ppo import plot_sharpe_ratio
+from backtest_ppo import plot_monthly_returns
+from backtest_ppo import monthly_retuns_hist
 
+# trade stocks using the equal weighting strategy
+def get_action(current_day_idx, data, target_weight): 
+    close = data['Close'] 
+    current_day = data.index[current_day_idx]
 
-    def feature_representation(self, state:int):
-        """
-        represent the feature of the state
-        We simply use tokenization
-        """
-        ## COMPLETE ##
-        # Write a function that get the state and returns the feature
-        # We simply use tokenization
-        allocation = self.env.observation_space['portfolio_allocation']
-        opens = self.env.observation_space['stock_opens']
-        feature[state] = 1
-        return feature
+    num_shares = target_weight / close[current_day:current_day]
+    num_shares = num_shares.values.flatten()
+    new_allocation = num_shares / LA.norm(num_shares, 1)
 
-
-    def play(self):                   
-        # Playing 
-        for episode in range(1, self.hp.num_test_episodes+1):         
-            state, _ = self.env.reset()
-            ended = False
-            truncated = False
-            step_size = 0
-            episode_reward = 0
-                                                           
-            while not ended and not truncated:
-                ## COMPLETE ##
-                # Find the feature of <state> using your implementation <self.feature_representation>
-                # Act greedy and find <action> using what you implemented in Class Agent
-                # state = self.feature_representation(state)
-                # action = self.agent.greedy(state)  
+    return new_allocation                 
 
 
-                
-                next_state, reward, ended, truncated, _ = self.env.step(action)
-                                
-                state = next_state
-                episode_reward += reward
-                step_size += 1
-                                                                                                                       
-            # Print Results of Episode            
-            printout = (f"Episode: {episode}, "
-                      f"Steps: {step_size:}, "
-                      f"Sum Reward of Episode: {episode_reward:.2f}, ")
-            print(printout)
-            
-        pygame.quit()
+
+if __name__ == "__main__":
+    investment = 100000
+    
+    testset_paths = sorted([os.path.join('data/test', f) for f in os.listdir('data/test') if not os.path.isfile(f)])
+    save_results_path = 'EQ_test_results'
+
+    portfolio_vals_sets = []
+    years = []
+    for i in range(len(testset_paths)):
+        file_name = testset_paths[i].split('/')[-1]
+        year = file_name.split('.')[0]
+
+        with open(testset_paths[i], 'rb') as f:
+            data = pickle.load(f)
+
+        env = StockTradingEnv(investment=investment, lookback_period=60,
+                            eta=1/252, dataset_path=testset_paths[i])
+        
+        num_companies = env.num_companies
+        target_weight = investment / num_companies
+        
+        current_day_idx = env.current_day_idx
+
+        # array representing each stock's fraction of the total portfolio
+        # initialize with equal allocation
+        portfolio_allocation = env._portfolio_allocation
+
+
+        done = False
+        state, _ = env.reset()
+
+
+        portfolio_vals = []
+        while not done:
+            action = get_action(current_day_idx, data, target_weight)
+            new_state, reward, done, _, _ = env.step(action)
+
+            portfolio_allocation = env._portfolio_allocation
+            current_day_idx = env.current_day_idx
+
+            portfolio_vals.append(env.portfolio_val)
+
+            # print("Day:", current_day_idx, 
+            #     "\nPortfolio Value:", env.portfolio_val,
+            #     "\nAllocation:", portfolio_allocation,
+            #     "\nReward:", reward, "\n\n")
+
+            state = new_state
+
+
+        print(f'portfolio value at the end of {year}: {portfolio_vals[-1]: .2f}')
+        years.append(year)
+        portfolio_vals_sets.append(portfolio_vals)
+
+    
+    annual_returns_plot_name = f'{save_results_path}/annual_returns'
+    annual_sharpe_plot_name = f'{save_results_path}/annual_sharpe_ratio'
+    monthly_returns_plot_name = f'{save_results_path}/monthly_returns'
+    monthly_returns_plot_name = f'{save_results_path}/monthly_returns'
+    monthly_returns_dist_name = f'{save_results_path}/monthly_returns_dist'
+    plot_annual_returns(portfolio_vals_sets, years, annual_returns_plot_name)
+    plot_sharpe_ratio(portfolio_vals_sets, years, annual_sharpe_plot_name)
+    plot_monthly_returns(portfolio_vals_sets, years, monthly_returns_plot_name)
+    monthly_retuns_hist(portfolio_vals_sets, path_to_save=monthly_returns_dist_name)
+
+    with open(f'{save_results_path}/portfolio_vals.pkl', 'wb') as f:
+        pickle.dump(portfolio_vals_sets, f)
