20a7c0ae4d481cf55aaf3c9e6dd6f4fa8c3caeb2
“mainbrm1380” <mainbrm1380@gmail.com>
2025-12-04 12:12:53 -0500

    added buy and hold

--- compare_results.py
+++ compare_results.py
@@ -13,6 +13,7 @@ ddpg_daily_vals_sets = 'DDPG_test_results/portfolio_vals.pkl'
 a2c_daily_vals_sets = 'A2C_test_results/portfolio_vals.pkl'
 acer_daily_vals_sets = 'ACER_test_results/portfolio_vals.pkl'
 EQ_daily_vals_sets = 'EQ_test_results/portfolio_vals.pkl'
+BH_daily_vals_sets = 'BH_test_results/portfolio_vals.pkl'
 
 comparison_graphs_path = 'comparison_graphs' # dir to save graphs
 os.makedirs(comparison_graphs_path, exist_ok=True)
@@ -33,7 +34,8 @@ data_dict = {
     'ddpg_val_sets':open_file(ddpg_daily_vals_sets),
     'a2c_val_sets':open_file(a2c_daily_vals_sets),
     'acer_val_sets':open_file(acer_daily_vals_sets),
-    'eq_val_sets':open_file(EQ_daily_vals_sets)
+    'eq_val_sets':open_file(EQ_daily_vals_sets),
+    'bh_val_sets':open_file(BH_daily_vals_sets)
 }
 
 
