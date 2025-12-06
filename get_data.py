bdd4abf459f1feddaa9065d3c5cffbfe3f82a386
“mainbrm1380” <mainbrm1380@gmail.com>
2025-12-06 15:27:54 -0500

    cleaned up the script

--- get_data.py
+++ get_data.py
@@ -44,38 +44,28 @@ def split_data(dataset_path, start_year='2005', end_year='2025', save_path='new_
         with open(f'{save_path}/test/{year}.pkl', 'wb') as f:
             pickle.dump(data_split, f)
 
+if __name__=='__main__':
 
+    # stocks are: apple, google, microsoft, amazon, nvidia
+    stock_list = ["AAPL", "GOOG", "MSFT", "AMZN", "JNJ"]
 
-# stocks are: apple, google, microsoft, amazon, nvidia
-# stock_list = ["AAPL", "META", "GOOG", "MSFT", "AMZN"]
-stock_list = ["AAPL", "GOOG", "MSFT", "AMZN", "JNJ"]
+    # 6000 thousand days of observations are collected, which is roughly equivalent to from January of 2005 to January of 2025.
+    start_date = "2005-01-01"
+    end_date = "2025-01-01"
 
-# 6000 thousand days of observations are collected, which is roughly equivalent to from January of 2005 to January of 2025.
-start_date = "2005-01-01"
-end_date = "2025-01-01"
+    # download the data
+    data = yfinance.download(stock_list, start=start_date, end=end_date)
 
-# download the data
-data = yfinance.download(stock_list, start=start_date, end=end_date)
+    # number of na vals in the data
+    missing_data = len(data[data.isna().any(axis=1)])
 
-# number of na vals in the data
-missing_data = len(data[data.isna().any(axis=1)])
+    # make sure there are no missing values
+    if missing_data > 0:
+        raise ValueError(f"There are missing values in the dataset!")
 
-# make sure there are no missing values
-if missing_data > 0:
-    raise ValueError(f"There are missing values in the dataset!")
+    # add the log return of each of the stocks
+    for stock in stock_list:
+        data["Log return", stock]= np.log(data["Close"][stock]).diff()
 
-# add the log return of each of the stocks
-for stock in stock_list:
-    data["Log return", stock]= np.log(data["Close"][stock]).diff()
-
-# print(len(data))  # there are 5033 data points for each stock
-# print(data.columns)
-
-# # save as csv
-# # data.to_csv("dataset.csv")
-
-# with open('new_dataset.pkl', 'wb') as f:
-#     pickle.dump(data, f)
-
-split_data('new_dataset.pkl', start_year='2005', end_year='2025', save_path='new_data')
+    split_data('new_dataset.pkl', start_year='2005', end_year='2025', save_path='new_data')
 
